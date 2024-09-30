6bd1316a2a5fcff7b9be06c238e008baefa4fadb
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-09-30 22:35:20 +0530

    mongoose connection

---
+++ config/Database.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose"
+
+export const connectDB = async () => {
+  mongoose.connect(process.env.MONGO_URL).then(() => console.log("MongoDB connected successfully"));
+};
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from 'dotenv';
 import userRouter from './routes/userRoute.js';
+import mongoose from "mongoose";
+import { connectDB } from "./config/Database.js";
 
 dotenv.config();
 
@@ -9,9 +11,13 @@ const app = express();
 // middleware
 app.use(express.json());
 
+// routes
 app.use('/api/v1', userRouter);
 
+// connect to db and start server
 const port = process.env.PORT || 8000;
 app.listen(port, () => { 
+  // connectDB();
+  mongoose.connect(process.env.MONGO_URL).then(() => console.log("MongoDB connected successfully")).catch(() => console.log("Couldn't connect"));
   console.log(`Server listening on ${port}`);
 });
